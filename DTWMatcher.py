c70d00eca86d9e8f13b42e338d60eb85cff7886f
Ivan FERNANDEZ <ivan.fernandez07@estudiant.upf.edu>
2023-05-08 16:20:51 +0200

    First implementation of matching

--- DTWMatcher.py
+++ DTWMatcher.py
@@ -10,6 +10,16 @@ class DTWMatcher:
         self.database = database
 
     def extract_features(self, audio_frame, sr=44100, stride=512, n_fft=2048):
+        """
+        Extracts the chroma features from the audio frame.
+        Params:
+            audio_frame: Audio frame to extract the features from.
+            sr: Sample rate of the audio frame.
+            stride: Hoplength for the chroma.
+            n_fft: FFT window size.
+            Returns:
+                features: Features extracted from the audio frame.
+        """
         chroma_features = librosa.feature.chroma_stft(y=audio_frame, sr=sr, tuning=0, norm=2,
                                                 hop_length=stride, n_fft=n_fft)
         
@@ -19,16 +29,43 @@ class DTWMatcher:
     
 
     def compute_dtw(self, audio_features, score_features):
+        """
+        Computes the DTW between the audio and score features using librosa's 
+        implementation for DTW.
+        Params:
+            audio_features: Features extracted from the audio frame.
+            score_features: Features extracted from the score frame.
+            Returns:
+                D: DTW matrix.
+                wp: Warping path.
+        """
         D, wp = librosa.sequence.dtw(X=audio_features, Y=score_features, metric='euclidean')
         return D,wp
 
     def average_distance(self,D, wp):
+        """
+        Computes the average distance between the audio and score features.
+        Params:
+            D: DTW matrix.
+            wp: Warping path.
+        Returns:
+            avg_distance: Average distance between the audio and score features.
+        """
         path_length = len(wp)
         distance = sum([D[wp[i][0], wp[i][1]] for i in range(path_length)])
         avg_distance = distance / path_length
         return avg_distance
     
     def identify_score(self, audio_frames):
+        """
+        Matches given query to all features in the database using DTW.
+        Params:
+            audio_frames: Audio frame to identify.
+            Returns:
+                results: List of tuples containing the score id and the average distance
+                to the input audio frame ordered in ascendent value. The lowest distance,
+                the most probable the match is.
+        """
         results = []
         count=0
         for score_id, score_features in self.database.items():
@@ -45,11 +82,15 @@ class DTWMatcher:
 #Outside class
 def create_database(MatcherInstance, audio_folder, duration=None):
     """
-    Creates a dictionary to store the hashed CQTs of the database.
+    Creates a dictionary to store the hashed chroma features of the database.
     Params:
         audio_folder: Path to the folder containing the audio files.
+        duration: Duration of the audio files wanted to be loaded.
+    Returns:
+        features_dict: Dictionary containing the hashed chroma features of the database.
+
     """
-    hashed_cqt_dict = {}
+    features_dict = {}
 
     for file_name in os.listdir(audio_folder):
         if file_name.endswith(".wav"):
@@ -65,36 +106,11 @@ def create_database(MatcherInstance, audio_folder, duration=None):
             id = id.split("_")[0]
             print(id, " ", features.shape)
 
-            hashed_cqt_dict[id] = features
+            features_dict[id] = features
 
     print("Feature dict created")
-    return hashed_cqt_dict
+    return features_dict
 
 
 
-
-
-audio_folder='./DiscoMIDI/data/synthesized_data'
-
-audio_folder='../../synthesized_midis/'
-file_paths=[]
-input_file_path = random_composition_performance()
-id = input_file_path.split(".")[0]
-id = id.split("/")[-1]
-print("Looking for ", input_file_path, "id: ", id)
-
-# Load the audio data from the input file
-input_audio, sr = librosa.load(input_file_path, sr=44100, duration=35)
-My_DTW_instance = DTWMatcher(database={})
-database = create_database(My_DTW_instance,audio_folder,duration=35)
-
-My_DTW_instance = DTWMatcher(database=database)
-input_features = My_DTW_instance.extract_features(input_audio, sr=44100)
-my_list=My_DTW_instance.identify_score(input_features)
-#print top 10
-for i in range(10):
-    print(my_list[i])
-print(id)
-print(my_list[0])
-#
 
--- test.py
+++ test.py
@@ -1,90 +1,36 @@
 from utils import *
-from pruning.prune import *
-from matching.matching import *
-
-#Testing some functions to get the needed data
-print(random_schubert_composition_performance())
-midi_files=get_synthesized_data_list()
-print(midi_files[3])
+import librosa
+import os
+from DTWMatcher import *
 
 
-#Testing the whole matching process!
+audio_folder='./data/synthesized_data'
 
-print("\n PRUNING STAGE \n")
-#The file we will look for will be:
-perf_path=random_composition_performance()
-full_perf_name=get_full_name(perf_path)
-print(full_perf_name)
-#First we need to create the dictionary of hashed cqt's
-synthesized_data_path='data/synthesized_data'
-"""
-Since I already created the dictionary, I will just load it
-hashed_cqt_dict=create_hashed_cqt_dict(synthesized_data_path)
+file_paths=[]
+input_file_path = random_composition_performance()
+id = input_file_path.split(".")[0]
+id = id.split("/")[-1]
+print("Looking for ", input_file_path, "id: ", id)
 
-#Now we need to process the cqt of the performance we want to find
-"""
-#load hashed_cqt_dict
-hashed_cqt_dict = pickle.load(open("hashed_cqt_dict.pk", "rb"))
-hashed_cqt = process_audio_file(perf_path)
-
-#Let's start counting the full matching process
-pruning_success=False
-start = time.time()
+# Load the audio data from the input file
+input_audio, sr = librosa.load(input_file_path, sr=44100, duration=35)
+# Create a DTWMatcher instance
+My_DTW_instance = DTWMatcher(database={})
+# This function returns a dictionary containing the chroma features of the database
+# The keys are the ids of the scores and the values are the chroma features
+database = create_database(My_DTW_instance,audio_folder,duration=35)
 
 
-top_matches = find_top_matches(hashed_cqt, hashed_cqt_dict)
-        
-    #print("Top 50 matches to ", get_full_name(input_audio_path), ":")
-for match in top_matches:
-    if get_full_name_id(match[0])==get_full_name(perf_path):
-            print("Match found!")
-            pruning_success=True
-            break
+My_DTW_instance = DTWMatcher(database=database)
+# Extract the features of the input audio
+input_features = My_DTW_instance.extract_features(input_audio, sr=44100)
+# Identify the score with the lowest distance to the input audio
+my_list=My_DTW_instance.identify_score(input_features)
 
-if pruning_success:
-    # start timer 
-    
+#print top 10
+for i in range(10):
+    print(my_list[i])
+print(id)
+print(my_list[0])
 
-    print("Pruning succesful!")
 
-    print("\nMATCHING STAGE\n")
-    #Let's get our filtered list containing the elements obtained by pruning
-    filtered_list=get_filtered_list(top_matches, midi_files)
-
-    results=matching(filtered_list, perf_path, 15)
-    results_names=[x[0] for x in results]
-    print(results_names[:5])
-    position=get_position(results_names, full_perf_name[0], full_perf_name[1])
-    print(position)
-    if position!=-1:
-        print("Matching succesful! :)")
-        #get list with only the names
-        
-        print("The performance was found in position ",position)
-    else:
-        print("Matching failed :(")
-    #stop timer 
-    end = time.time()
-    # total time taken
-    print(f"Runtime of the program is {end - start}")
-else:
-    print("Pruning failed :(")
-    print("\nMATCHING STAGE\n")
-
-    #Sadly we have to do the matching with the whole list
-    results=matching(midi_files, perf_path, 35) 
-    results_names=[x[0] for x in results]
-    print(results_names[:5])
-    position=get_position(results_names, full_perf_name[0], full_perf_name[1])
-    print(position)
-    if position!=-1:
-        print("Matching succesful! :)")
-        #get list with only the names
-        
-        print("The performance was found in position ", position)
-    else:
-        print("Matching failed :(")
-    #stop timer
-    end = time.time()
-    # total time taken
-    print(f"Runtime of the program is {end - start}")
