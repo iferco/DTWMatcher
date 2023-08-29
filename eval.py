2d0be1c3b905bc17b7e11d5f13defd838959229f
iferco <118919199+iferco@users.noreply.github.com>
2023-08-29 23:06:53 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -50,33 +50,6 @@ def mean_top_10_pos(list_match_pos):
             top_10_pos.append(pos) 
     return np.mean(top_10_pos)
 
-
-
-
-def ndcg_at_k(positions, k=10):
-    """
-    Calculate Normalized Discounted Cumulative Gain (NDCG) at rank k.
-    Parameters:
-        positions: list of positions of the correct match for different iterations
-        k: the rank cutoff
-    Returns:
-        float, the average NDCG at rank k
-    """
-    ndcgs = []
-    for pos in positions:
-        if pos < 0:  # no match found in top k positions
-            ndcgs.append(0)
-        elif pos < k:  # adjusting for 0-indexing
-            dcg = 1 / np.log2(pos + 1 + 1)  # pos + 1 for 0-indexing, and another + 1 because log is undefined at 0
-            idcg = 1  # Ideal DCG is 1 because the relevance of the correct match is 1
-            #TODO Should idcg be 1 just for items in pos 0? What values to set for the rest?
-
-            ndcgs.append(dcg / idcg)
-        else:  # pos >= k
-            ndcgs.append(0)
-    return np.mean(ndcgs)
-
-
 def calculate_p_at_10(match_positions, iterations=100):
     """
     Calculate Precision at rank 10 (P@10).
